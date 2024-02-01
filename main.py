a2509cd03666901f96b6b98c54e22436431fe470
pauldak <78545563+pauldak@users.noreply.github.com>
2024-02-01 22:58:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import streamlit as st
 
-# Triggered by a button click
 if st.button('Click to copy "Hello, World!" to clipboard'):
-    # JavaScript to copy text
     js_script = """
-        <script>
-        navigator.clipboard.writeText('Hello, World!').then(function() {
-            /* Success */
-            alert('Text copied successfully!');
-        }, function(err) {
-            /* Error */
-            console.error('Could not copy text: ', err);
-        });
-        </script>
+    <script defer>
+    navigator.clipboard.writeText('Hello, World!').then(function() {
+      alert('Text copied successfully!');
+    }, function(err) {
+      console.error('Could not copy text: ', err);
+    });
+    </script>
     """
-
-    # Display the JavaScript
-    st.markdown(js_script, unsafe_allow_html=True)
+    st.write(js_script, unsafe_allow_html=True)
