1ddbd13a16ea88e796b66b6e057bdb53008d23e2
pauldak <78545563+pauldak@users.noreply.github.com>
2024-02-02 12:21:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
-import streamlit.components.v1 as components
-
-components.iframe("https://cdn.jsdelivr.net/npm/@streamlit/styletron-provider@0.1.0/build/bundle.js", height=0)
 
 if st.button('Click to copy "Hello, World!" to clipboard'):
     js_script = """
-    <script defer>
+    <script>
         navigator.clipboard.writeText('Hello, World!').then(function() {
-            st.success('Text copied successfully!');
+            alert('Text copied successfully!');
         }, function(err) {
-            st.error('Failed to copy text: ' + err);
+            alert('Failed to copy text: ' + err);
         });
     </script>
     """
-    st.write(js_script, unsafe_allow_html=True)
+    st.markdown(js_script, unsafe_allow_html=True)
