69114d585c47a0cdaea5f442fb262b2a2ddb79e5
pauldak <shir.armony@mail.huji.ac.il>
2024-02-02 12:10:36 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+components.iframe("https://cdn.jsdelivr.net/npm/@streamlit/styletron-provider@0.1.0/build/bundle.js", height=0)
 
 if st.button('Click to copy "Hello, World!" to clipboard'):
     js_script = """
     <script defer>
-    navigator.clipboard.writeText('Hello, World!').then(function() {
-      st.success('Text copied successfully!');
-    }, function(err) {
-      st.error('Failed to copy text: ' + err);
-    });
+        navigator.clipboard.writeText('Hello, World!').then(function() {
+            st.success('Text copied successfully!');
+        }, function(err) {
+            st.error('Failed to copy text: ' + err);
+        });
     </script>
     """
     st.write(js_script, unsafe_allow_html=True)
