77356fa1655a2e44d50ee6d139a1c947ff1acb2d
pauldak <shir.armony@mail.huji.ac.il>
2024-02-03 12:44:48 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 def main():
-    st.title("Copy Paste in streamlit")
-    prompt = "I just want to test it"
+    st.title("Hit the copy at the bottom right of the prompt")
+    prompt = ("I just want to test it bbvbcbvbn vbnbnvnbnv  cbnvncbvcnbbvbn "
+              "eyuey uuiruyy vcnvcnif m iiiieiei  "
+              "vncvn c v  vc vc  vccv cv fgg f gfgfg")
     # pathinput = st.text_input(prompt)
 
     #you can place your path instead
