6e8850423858c0b51a76636cf193b696677a4a2a
pauldak <78545563+pauldak@users.noreply.github.com>
2024-02-03 12:36:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def main():
     st.title("Copy Paste in streamlit")
     prompt = "I just want to test it"
     pathinput = st.text_input(prompt)
-    
+
     #you can place your path instead
-    Path = f'''{pathinput}'''
+    Path = f'''{prompt}'''
     st.code(Path, language="python")
     st.markdown("Now you get option to copy")
 if __name__ == "__main__":main()
