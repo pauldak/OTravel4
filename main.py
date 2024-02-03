9e063197907d9df931d3d07b993f1273087b402d
pauldak <78545563+pauldak@users.noreply.github.com>
2024-02-03 11:58:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 def main():
     st.title("Copy Paste in streamlit")
-    pathinput = st.text_input("Enter your Path:")
+    # pathinput = st.text_input("Enter your Path:")
+    pathinput = "Just test clipboard"
     #you can place your path instead
     Path = f'''{pathinput}'''
     st.code(Path, language="python")
